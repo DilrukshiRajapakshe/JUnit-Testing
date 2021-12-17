5c45e3241550c1dd463923369f269dc5556273e9
Maduranga Namal <maduranga.wimalarathne95@gmail.com>
2021-12-17 14:11:35 +0530

    deleted dir

--- pos-backend/src/main/java/com/maduranga/posbackend/dao/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.maduranga.posbackend.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.maduranga.posbackend.model.Category;
-
-public interface CategoryRepo extends CrudRepository<Category, String> {
-
-}
